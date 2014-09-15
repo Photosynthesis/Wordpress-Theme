cb558fed2661503252a929866ed143c41a33d641
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-09-15 02:25:12 -0400

    Add Template For CoHousing RSS Feed

---
+++ coho-rss-template.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: CoHousing US RSS Template
+ */
+
+header('Content-Type: application/rss+xml; charset=UTF-8');
+echo FrmProDisplaysController::get_shortcode(array('id' => 149232))
+
+?>
