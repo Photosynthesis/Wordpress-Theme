1cfcac4f77023151fcd8f5cfd01ead1d8e832edc
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-11-09 04:15:21 -0500

    Add Person Name and Year Listed to Export Script
    Modify the listing_email_export script to include the name and year
    listed. Also, we now only fallback to the editor if the contact email is
    invalid.

--- scripts/listing_email_export.py
+++ scripts/listing_email_export.py
@@ -37,8 +37,14 @@ EMAIL_REGEX = re.compile(r'[^@]+@[^@]+\.[^@]+')
 # The ID Number of the Formidable Form
 FORM_ID = 2
 
-# The ID Number of the Formidable Field Containg the Contact Person's Email.
+# The ID Number of the Formidable Field Containing the Contact Person's Email.
 CONTACT_EMAIL_FIELD_ID = 199
+
+# The ID Number of the Formidable Field Containg the Contact Person's Name.
+CONTACT_NAME_FIELD_ID = 202
+
+# The ID Number of the Formidable Field of the Year the Listing was Made
+LISTING_CREATED_FIELD_ID = 725
 
 
 def main():
@@ -59,11 +65,21 @@ def get_listing_contact_rows():
                    FROM 3uOgy46w_frm_item_metas
                    WHERE field_id={1})
              AS cemail_metas ON items.id=cemail_metas.item_id
+        LEFT JOIN (SELECT meta_value AS contact_name, item_id
+                   FROM 3uOgy46w_frm_item_metas
+                   WHERE field_id={2})
+             AS cname_metas ON items.id=cname_metas.item_id
+        LEFT JOIN (SELECT meta_value AS created_date, item_id
+                   FROM 3uOgy46w_frm_item_metas
+                   WHERE field_id={3})
+             AS created_metas ON items.id=created_metas.item_id
         LEFT JOIN (SELECT post_title, post_author, ID FROM 3uOgy46w_posts)
              AS posts ON items.post_id=posts.ID
-        LEFT JOIN (SELECT user_email, ID FROM 3uOgy46w_users)
+        LEFT JOIN (SELECT user_email, display_name, ID FROM 3uOgy46w_users)
             AS users ON users.ID=posts.post_author
-        WHERE items.form_id={0}""".format(FORM_ID, CONTACT_EMAIL_FIELD_ID)
+        WHERE items.form_id={0}""".format(FORM_ID, CONTACT_EMAIL_FIELD_ID,
+                                          CONTACT_NAME_FIELD_ID,
+                                          LISTING_CREATED_FIELD_ID)
     cursor = get_cursor()
     cursor.execute(listing_contacts_query)
     listing_rows = cursor.fetchall()
@@ -87,15 +103,20 @@ def make_csv_line(listing_row):
     """Create a CSV line of ``email,community name,role`` from a Listing row."""
     output = ""
     community_name = clean(listing_row["post_title"])
+    created_years = clean_date(listing_row["created_date"])
 
     contact_email = clean(listing_row["contact_email"])
     if is_valid_email(contact_email):
-        output += u"{0},{1},contact\r\n".format(contact_email, community_name)
-
-    editor_email = clean(listing_row["user_email"])
-    if is_valid_email(editor_email):
-        if editor_email != contact_email:
-            output += u"{0},{1},editor\r\n".format(editor_email, community_name)
+        contact_name = clean_name(listing_row["contact_name"])
+        output += u"{0},{1},{2},{3},contact\r\n".format(
+            contact_email, contact_name, created_years, community_name)
+    if contact_email in [None, '']:
+        editor_email = clean(listing_row["user_email"])
+        if is_valid_email(editor_email):
+            editor_name = clean_name(listing_row["display_name"])
+            if editor_email != contact_email:
+                output += u"{0},{1},{2},{3},editor\r\n".format(
+                    editor_email, editor_name, created_years, community_name)
     return output
 
 
@@ -106,11 +127,36 @@ def clean(field_value):
     return field_value.replace(',', '').replace("'", '').replace('"', '')
 
 
+def clean_date(date_string):
+    """Validate a YYYY-MM-DD string and return the Year or an empty string."""
+    if date_string is None:
+        return ''
+    parts = date_string.split('-')
+    if len(parts) != 3:
+        return ''
+    try:
+        year = int(parts[0])
+        return year if year != 1970 else ''
+    except ValueError:
+        return ''
+
+
 def is_valid_email(email_address):
     """Check if an E-Mail Address is Valid."""
     return (email_address is not None and
             ' ' not in email_address and
             EMAIL_REGEX.match(email_address) is not None)
+
+
+def clean_name(name):
+    """Remove the last name."""
+    if name is None:
+        return ''
+    parts = name.split(' ')
+    if len(parts) > 1:
+        return ' '.join(parts[:-1])
+    else:
+        return name
 
 
 def ensure_unique_emails(csv_lines):
@@ -131,6 +177,7 @@ def write_csv_file(csv_lines):
     """Write the Lines to ``export.csv`` in the Current Working Directory."""
     with open('./export.csv', 'w') as output_file:
         output_file.writelines(s.encode('UTF-8') for s in csv_lines)
+
 
 if __name__ == "__main__":
     main()
