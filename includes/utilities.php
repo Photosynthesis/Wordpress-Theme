4535d8a6d4cc2963179f0651bdf6d25ec1c5e0db
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-06-01 07:39:05 -0400

    Refactor Utilities into a Class

--- includes/utilities.php
+++ includes/utilities.php
@@ -2,27 +2,30 @@
 /** Utility Functions for FIC's Wordpress Site
  *
  * @category FIC
- * @package  Utilities
+ * @package  FIC_Utils
  * @author   Pavan Rikhi <pavan@ic.org>
  * @license  GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
  * @link     http://www.ic.org
  */
 
 
-/** HTML-Escape Ampersands
- *
- * Takes one parameter, `content`.
- *
- * @param array $atts The Shortcode Parameter
- *
- * @return string The HTML-escaped content.
- */
-function escape_ampersands($atts)
+class FIC_Utils
 {
-    extract(shortcode_atts(array('content' => ''), $atts));
-    $escaped_content = str_replace('&', '&amp;', $content);
-    return $escaped_content;
+    /** HTML-Escape Ampersands
+     *
+     * Takes one parameter, `content`.
+     *
+     * @param array $atts The Shortcode Parameter
+     *
+     * @return string The HTML-escaped content.
+     */
+    public static function escape_ampersands($atts) {
+        extract(shortcode_atts(array('content' => ''), $atts));
+        $escaped_content = str_replace('&', '&amp;', $content);
+        return $escaped_content;
+    }
 }
-add_shortcode('escape_ampersands', 'escape_ampersands');
+add_shortcode('escape_ampersands', array('FIC_Utils', 'escape_ampersands'));
+
 
 ?>
