088267afbc252cd21483f0f8139f2b3dc4189641
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-05-31 23:51:05 -0400

    Add Newest Coho RSS & Move Templates to Subfolder

---
+++ templates/newest-cohousing-communities-rss.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Newest US Cohousing Communities RSS Template
+ */
+
+header('Content-Type: application/rss+xml; charset=UTF-8');
+echo FrmProDisplaysController::get_shortcode(array('id' => 174965));
+
+?>
