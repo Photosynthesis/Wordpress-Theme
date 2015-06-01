12dcb0cd0932acf35133482d2c0ea6bb38c698e1
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-06-01 07:41:18 -0400

    [#567] Fix Country List Names & Counts
    Rewrite the show_directory_geo_list shortcode, making the names
    normalized to capitalized words & the community counts accurate.
    
    Fixes #567: State / Province in Country List

--- includes/directory_listings.php
+++ includes/directory_listings.php
@@ -1,7 +1,7 @@
 <?php
 /** General Functions for Formidable Directory Listings
  *
- * This file contains general functions for Directory Listings.
+ * This file contains general functions & shortcodes for Directory Listings.
  *
  * @category FIC
  * @package  Formidable_Directory
@@ -71,5 +71,122 @@ add_shortcode(
     'directory_show_message_if_404_in_directory',
     'directory_show_message_if_404_in_directory'
 );
+
+
+/** Show multiple lists of Communities, drilling down by Country and then
+ * State/Province.
+ *
+ * US Listings are shown first.
+ *
+ * @return string HTML containing the lists
+ */
+function directory_show_directory_geo_list() {
+    delete_transient('directory_geographical_lists');
+    return FIC_Utils::cache_result(function() {
+        global $wpdb;
+
+        $directory_form_id = 2;
+        $is_public_field_id = 218;
+        $country_field_id = 424;
+        //$region_field_id = 428;  // Has less entries
+        $region_field_id = 816;
+        $state_field_id = 815;
+        $entries_query = "
+            SELECT countries.country, regions.region, states.state
+            FROM `{$wpdb->prefix}frm_items` AS entries
+            " . /*  Commented out in order to keep the Counts accurate
+                    until hiding drafts works in Formidable
+            INNER JOIN (SELECT ID, post_status
+                        FROM `{$wpdb->prefix}posts`
+                        WHERE post_status='publish')
+                   AS posts ON posts.ID=entries.post_id
+            */ "
+            INNER JOIN (SELECT `item_id`, `meta_value` AS is_public
+                        FROM `{$wpdb->prefix}frm_item_metas`
+                        WHERE `meta_value`='Yes' AND
+                              `field_id`=$is_public_field_id)
+                   AS public ON public.item_id=entries.id
+            INNER JOIN (SELECT `item_id`, `meta_value` AS country
+                        FROM `{$wpdb->prefix}frm_item_metas`
+                        WHERE `field_id`=$country_field_id)
+                   AS countries ON countries.item_id=entries.id
+            LEFT JOIN (SELECT `item_id`, `meta_value` AS region
+                       FROM `{$wpdb->prefix}frm_item_metas`
+                       WHERE `field_id`=$region_field_id)
+                   AS regions ON regions.item_id=entries.id
+            LEFT JOIN (SELECT `item_id`, `meta_value` AS state
+                       FROM `{$wpdb->prefix}frm_item_metas`
+                       WHERE `field_id`=$state_field_id)
+                   AS states ON states.item_id=entries.id
+            WHERE entries.form_id=$directory_form_id
+            ;";
+        $entries = $wpdb->get_results($entries_query);
+
+        $countries = array();
+        // Count Entries
+        foreach ($entries as $entry) {
+            if (!array_key_exists($entry->country, $countries)) {
+                $countries[$entry->country] = array();
+            }
+            $region = $entry->country === "United States" ?
+                      $entry->state : ucwords($entry->region);
+            if (!array_key_exists($region, $countries[$entry->country])) {
+                $countries[$entry->country][$region] = 1;
+            } else {
+                $countries[$entry->country][$region]++;
+            }
+        }
+
+        // Sort Countries & Regions Alphabetically
+        foreach ($countries as &$country) {
+            ksort($country);
+        }
+        ksort($countries);
+
+        // Move US to the Top
+        $us = $countries["United States"];
+        unset($countries["United States"]);
+        $countries = array_merge(array("United States" => $us), $countries);
+
+        // Render as HTML
+        $countries_html = '';
+        foreach ($countries as $country => $regions) {
+            $country_total = array_sum($regions);
+            $country_url = "/directory/listings/?cmty-country=$country";
+            if ($country === "United States")  {
+                $country_class = 'geo-us-state';
+                $region_get_parameter = 'cmty-state';
+            } else {
+                $country_class = 'geo-state';
+                $region_get_parameter = 'cmty-prov';
+            }
+            $region_html = '';
+            foreach ($regions as $region => $count) {
+                if (empty($region)) {
+                    continue;
+                } else if ($region === "Kentucky" || $region === "Ohio") {
+                    // Start a New Column
+                    $region_html .= "</ul></li><li class='$country_class'>
+                                     <ul class='$country_class'>";
+                }
+                $region_url = "$country_url&$region_get_parameter=$region";
+                $region_html .= "
+                    <li class='geo-state-prov'><a href='$region_url'>
+                        $region <span class='geo-count'>($count)</span>
+                    </a></li>";
+            }
+
+            $countries_html .= "
+                <li class='geo-country'><a href='$country_url'>$country
+                    <span class='geo-count'>($country_total)</span></a>
+                    <ul class='$country_class'>$region_html</ul>
+                </li>";
+        };
+
+        return '<ul class="geo-country">' . $countries_html . '</ul>';
+    }, 'directory_geographical_lists', 1);
+}
+add_shortcode('show_directory_geo_list', 'directory_show_directory_geo_list');
+
 
 ?>
