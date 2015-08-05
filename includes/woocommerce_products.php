05324517be11d9a9a287aa5398b9dd688829d92c
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-08-04 20:20:07 -0400

    [#626] Hide Prices for Free Products
    Closes #626

--- includes/woocommerce_products.php
+++ includes/woocommerce_products.php
@@ -14,7 +14,8 @@ function hide_suggested_prices($price, $product)
 {
     $is_suggested_price_product = WC_Name_Your_Price_Helpers::is_nyp($product);
     // `&#36;` is the HTML entity for `$`
-    $price_is_zero = strpos($price, '&#36;0.00') !== false;
+    $price_is_zero =
+        (strpos($price, '&#36;0.00') || strpos($price, 'Free!')) !== false;
     if ($is_suggested_price_product || $price_is_zero) {
         $price = '';
     }
