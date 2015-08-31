39e297ef44f7259e1c771ca595661036b1f2bb01
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-08-30 20:53:47 -0400

    [#22] Fix Directory RSS Feeds
    Use custom templates to display the Formidable views for the Communies
    Directory's RSS feeds.
    
    Closes #22

---
+++ templates/communities-rss-updated.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: Recently Updated Communities RSS Template
+ */
+
+header('Content-Type: application/rss+xml; charset=UTF-8');
+echo FrmProDisplaysController::get_shortcode(array('id' => 135680));
+
+?>
