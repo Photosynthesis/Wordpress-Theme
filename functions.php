43d1f6b72452f131451e1c0b484e89ab479b4bed
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-05-07 17:07:29 -0400

    Enable Lightboxes on Product Detail Pages

--- functions.php
+++ functions.php
@@ -145,6 +145,7 @@ add_filter('excerpt_more', 'theme_excerpt_more');
 add_action('after_setup_theme', 'woocommerce_support');
 function woocommerce_support() {
   add_theme_support('woocommerce');
+  add_theme_support('wc-product-gallery-lightbox');
 }
 /* Disable WooCommerce CSS */
 add_filter('woocommerce_enqueue_styles', '__return_false');
