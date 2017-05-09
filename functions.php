770f0dd43923d8ad1cba3fc13bfb9b2a9102effb
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-05-08 21:02:08 -0400

    Customize the Directory List & Detail Pages

--- functions.php
+++ functions.php
@@ -140,6 +140,110 @@ add_filter('excerpt_more', 'theme_excerpt_more');
 
 
 
+/** Directory **/
+/* Don't Automatically Create Paragraphs From Newlines */
+//add_filter('frm_use_wpautop', create_function('', "return false;"));
+
+/** Return an Edit link for the Directory Listing if the Current User is an
+ * Administrator. This is checked by checking for the `edit_plugins`
+ * permission.
+ *
+ * One shortcode parameter is required: `listing_id`, which determines what
+ * Edit Link is returned.
+ *
+ * @param array $atts The Shortcode Parameter
+ *
+ * @return string The Directory Listing's Edit Link in HTML
+ */
+function directory_show_edit_link_if_admin($atts)
+{
+    extract(shortcode_atts(array('listing_id' => 0), $atts));
+    if (current_user_can('edit_plugins')) {
+        $entry_edit_url = "/wp-admin/admin.php" .
+            "?page=formidable-entries&frm_action=edit&id=$listing_id";
+        return "<a href=\"$entry_edit_url\">Edit Listing</a>";
+    } else {
+        return "";
+    }
+}
+add_shortcode(
+    'directory_show_edit_link_if_admin', 'directory_show_edit_link_if_admin'
+);
+
+/** Return a link for marking a Directory Listing as up-to-date if the current
+ * user is an admin or the listings editor. If the current page has an update
+ * request in it's GET parameters, this shortcode will set the Formidable
+ * Entry's Last Verified field to today, with the same restrictions as for
+ * displaying the link.
+ *
+ * Admins are users that have the 'edit_plugins' permission.
+ *
+ * Two parameters are required:
+ *
+ * 1. `listing_id` - the formidable entry's ID
+ * 2. `post_id` - the formidable entry's post's ID
+ *
+ * @param array $atts The Shortcode Parameters
+ *
+ * @return string The Directory Listing's Verify Link in HTML
+ */
+function directory_show_and_process_verify_link($atts)
+{
+    global $wpdb;
+    extract(shortcode_atts(array('listing_id' => 0, 'post_id' => 0), $atts));
+    $post_id = intval($post_id);
+    $listing_id = intval($listing_id);
+    if ($post_id === 0 || $listing_id === 0) { return ''; }
+
+    $editor_id = get_post($post_id)->post_author;
+    $current_user = get_current_user_id();
+
+    if (current_user_can('edit_plugins') || $editor_id == $current_user) {
+        $get_parameter = 'verify_as_up_to_date';
+
+        if (isset($_GET[$get_parameter])) {
+            $entry_is_valid = directory_validate_entry($listing_id);
+            if (!$entry_is_valid) {
+                return '<small style="color:red;text-emphasis:bold;">' .
+                    'Your Listing could not be verified <br />' .
+                    'because it is incomplete, please edit your <br />' .
+                    'listing before verifying it.</small>';
+            }
+            $verify_date_field_id = 937;
+            $exists_query = "
+                SELECT id FROM {$wpdb->prefix}frm_item_metas
+                WHERE `field_id`=$verify_date_field_id
+                  AND `item_id`=$listing_id;";
+            $results = $wpdb->get_results($exists_query);
+            $result_count = $wpdb->num_rows;
+            $today = date('Y-m-d');
+            if ($result_count === 0) {
+                $insert_query = "
+                    INSERT INTO {$wpdb->prefix}frm_item_metas
+                            (meta_value, field_id, item_id, created_at)
+                    VALUES  ('$today', $verify_date_field_id, $listing_id, NOW());";
+                $wpdb->get_results($insert_query);
+            } else {
+                $meta_id = $results[0]->id;
+                $update_query = "
+                    UPDATE `{$wpdb->prefix}frm_item_metas`
+                    SET meta_value='$today'
+                    WHERE `id`=$meta_id;";
+                $wpdb->get_results($update_query);
+            }
+            return '<b>Listing Successfully Verfied.</b>';
+        }
+        return "<a href='.?$get_parameter=1'>Verify as Up-to-Date</a>";
+    }
+
+    return '';
+}
+add_shortcode(
+    'directory_verify_listing_link', 'directory_show_and_process_verify_link'
+);
+
+
+
 /** WooCommerce **/
 /* Woocommerce Hooks */
 add_action('after_setup_theme', 'woocommerce_support');
