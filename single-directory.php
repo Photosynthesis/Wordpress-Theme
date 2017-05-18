407ca6f7141aa8735b82df34e81543ebd720233c
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-05-17 21:07:28 -0400

    Remove Banner From Directory Pages

--- single-directory.php
+++ single-directory.php
@@ -5,10 +5,6 @@ remove_filter('the_content', 'wpautop');
 
 theme_top(); ?>
 
-<a class='mb-3 d-block text-center' href='/fall-fundraising-campaign'>
-  <img class='img-fluid' src='/wp-content/uploads/2016/12/FundraiserBanner5.png' />
-</a>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <div><?php the_content(); ?></div>
 </article>
