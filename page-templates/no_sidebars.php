d56be4dabe324a3382e74f12cb2a9729e4079faa
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-07-28 21:48:16 -0400

    Add "No Sidebars" Page Template

---
+++ page-templates/no_sidebars.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Template Name: No Sidebars
+ */
+
+get_header(); ?>
+
+<div class='row'>
+  <div class='col mt-2'>
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class='clearfix'><?php
+          if (has_post_thumbnail()) {
+            $thumbnail = get_the_post_thumbnail();
+            if ($thumbnail != '') { ?>
+              <div class="float-left mr-2 mb-2">
+                <div class="card"><a href="<?php the_permalink(); ?>">
+                  <?php echo $thumbnail; ?>
+                </a></div>
+              </div><?php
+            }
+          } ?>
+        </div>
+
+      <div><?php the_content(); ?></div>
+
+    </article>
+  </div>
+</div>
+<?php
+
+get_footer(); ?>
