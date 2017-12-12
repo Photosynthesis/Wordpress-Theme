aec4cb122e6ccae79765fb3b352f2ff6476fc244
Pavan Rikhi <pavan.rikhi@gmail.com>
2017-12-12 15:51:43 -0500

    Fix Hustle Form Parsing

--- includes/newsletter.php
+++ includes/newsletter.php
@@ -6,32 +6,32 @@ class ThemeNewsletter
   public static function hustle_opt_in_submit() {
     global $hustle;
 
-    parse_str($_POST['data'], $raw_data);
-    if (!is_email($raw_data['inc_optin_email'])) {
-      return;
-    }
-    $form_data = array();
-    foreach ($raw_data as $key => $value) {
+    $raw_data = array();
+    parse_str($_POST['data']['form'], $raw_data);
+    if (is_email($raw_data['inc_optin_email'])) {
+      $form_data = array();
+      foreach ($raw_data as $key => $value) {
       if (preg_match('%inc_optin_%', $key)) {
-        $key = str_replace('inc_optin_', '', $key);
+          $key = str_replace('inc_optin_', '', $key);
       }
       $form_data[$key] = $value;
-    }
+      }
 
-    $user_data = array('email' => $form_data['email']);
-    if (isset($form_data['first_name'])) {
+      $user_data = array('email' => $form_data['email']);
+      if (isset($form_data['first_name'])) {
       $user_data['firstname'] = $form_data['first_name'];
-    }
-    if (isset($form_data['last_name'])) {
+      }
+      if (isset($form_data['last_name'])) {
       $user_data['lastname'] = $form_data['last_name'];
-    }
+      }
 
-    $mailpoet_data = array(
+      $mailpoet_data = array(
       'user' => $user_data,
       'user_list' => array('list_ids' => array('1')),
-    );
-    $mp_helper = WYSIJA::get('user', 'helper');
-    $mp_helper->addSubscriber($mailpoet_data);
+      );
+      $mp_helper = WYSIJA::get('user', 'helper');
+      $mp_helper->addSubscriber($mailpoet_data);
+    }
 
     $hustle_ajax = new Opt_In_Front_Ajax($hustle);
     $hustle_ajax->submit_optin();
