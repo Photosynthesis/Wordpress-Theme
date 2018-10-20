d27d8d521c2a2f18f72a361eecb5d4ba855f7791
Pavan Rikhi <pavan.rikhi@gmail.com>
2018-10-19 21:20:25 -0400

    Fix WooCommerce Templates

--- woocommerce/single-product/product-image.php
+++ woocommerce/single-product/product-image.php
@@ -32,7 +32,7 @@ $wrapper_classes   = apply_filters( 'woocommerce_single_product_image_gallery_cl
     <?php
 
     if ( has_post_thumbnail() ) {
-      $html  = wc_get_galley_image_html( $post_thumbnail_id, true );
+      $html  = wc_get_gallery_image_html( $post_thumbnail_id, true );
     } else {
       $html  = '<div class="woocommerce-product-gallery__image--placeholder">';
       $html .= sprintf( '<img src="%s" alt="%s" class="img-fluid wp-post-image" />', esc_url( wc_placeholder_img_src() ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
