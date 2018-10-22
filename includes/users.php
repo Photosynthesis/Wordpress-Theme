b9c8628b2e4b1190f5b94c59cb11e42753c689ad
Pavan Rikhi <pavan.rikhi@gmail.com>
2018-10-22 09:07:33 -0400

    [#308] Add User ADB Field & Order/Subscription Meta Boxes
    This adds an `fic_customer_adb` user meta for customer's offline
    database reference numbers. This is hidden from users, editable only
    from the admin.
    
    Meta boxes are also added on the Order & Subscription edit pages,
    allowing managers to modify them when processing orders.
    
    Fixes #308

--- includes/users.php
+++ includes/users.php
@@ -60,6 +60,97 @@ class ThemeUsers
   public static function lost_password_url($lostpassword_url, $redirect) {
     return home_url() . '/wp-login.php?action=lostpassword' . $redirect;
   }
+
+  const adb_meta_key = 'fic_customer_adb';
+  const adb_meta_box_id = 'fic_adb_box';
+  /* Show an ADB field when editing a User */
+  public static function show_adb_input($user) {
+    $adb_number = get_user_meta($user->ID, self::adb_meta_key, true);
+    $val = esc_attr($adb_number);
+    $output = <<<HTML
+<h3>ADB Number</h3>
+<table class='form-table'>
+  <tr>
+    <th><label for="adb_number">ADB Number</label></th>
+    <td>
+      <input
+        type="number"
+        min="0"
+        max="99999"
+        step="1"
+        id="adb_number"
+        name="adb_number"
+        value="{$val}"
+        class='regular-text'
+      />
+    </td>
+  </tr>
+</table>
+HTML;
+    echo $output;
+  }
+
+  /* Update the ADB field for a User */
+  public static function update_user_adb($user_id) {
+    if (!current_user_can('edit_user', $user_id)) {
+      return false;
+    }
+    if (!empty($_POST['adb_number'])) {
+      update_user_meta($user_id, self::adb_meta_key, intval($_POST['adb_number']));
+    }
+  }
+
+  /* Add the ADB Number meta box to the Order & Subscription pages */
+  public static function add_adb_meta_box() {
+    add_meta_box(
+      self::adb_meta_box_id,
+      'ADB Number',
+      array('ThemeUsers', 'render_adb_meta_box'),
+      array('shop_order', 'shop_subscription'),
+      'side',
+      'core'
+    );
+  }
+
+  /* Render the ADB Number meta box */
+  public static function render_adb_meta_box() {
+    global $post;
+    $user_id = wc_get_order($post->ID)->get_user_id();
+    $adb_number = get_user_meta($user_id, self::adb_meta_key, true);
+    $nonce = wp_create_nonce('adb-meta-box');
+    $output = <<<HTML
+<input type='hidden' name='adb_nonce' value='{$nonce}' />
+<input
+  type="number"
+  min="0"
+  max="99999"
+  step="1"
+  id="adb_number"
+  name="adb_number"
+  value="{$adb_number}"
+/>
+HTML;
+    echo $output;
+  }
+
+  /* Save the ADB Number on updates */
+  public static function update_adb_meta_box($post_id) {
+    if (!isset($_POST['adb_nonce'])) {
+      return $post_id;
+    }
+    $nonce = $_REQUEST['adb_nonce'];
+    if (!wp_verify_nonce($nonce, 'adb-meta-box')) {
+      return $post_id;
+    }
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+      return $post_id;
+    }
+    if (!current_user_can('edit_post', $post_id)) {
+      return $post_id;
+    }
+    $user_id = wc_get_order($post_id)->get_user_id();
+    update_user_meta($user_id, self::adb_meta_key, intval($_POST['adb_number']));
+  }
 }
 
 add_action('wp_logout', array('ThemeUsers', 'logout_redirect'));
@@ -68,5 +159,10 @@ add_filter('login_redirect', array('ThemeUsers', 'login_redirect'), 10, 3);
 add_shortcode('logged_in_only', array('ThemeUsers', 'logged_in_only'));
 add_shortcode('display_login_form', array('ThemeUsers', 'login_form'));
 add_filter('lostpassword_url', array('ThemeUsers', 'lost_password_url'), 10, 2);
+
+add_action('edit_user_profile', array('ThemeUsers', 'show_adb_input'));
+add_action('edit_user_profile_update', array('ThemeUsers', 'update_user_adb'));
+add_action('add_meta_boxes', array('ThemeUsers', 'add_adb_meta_box'));
+add_action('save_post', array('ThemeUsers', 'update_adb_meta_box'));
 
 ?>
