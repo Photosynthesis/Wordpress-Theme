dd69923299e39280211a7d91780b4cd0dc2e9db6
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-04-11 20:19:28 -0400

    Update Footer Content & Styling
    Remove the Membership & Partners sections from the footer, adding a
    Support section, 3 column mission/pages/newsletter section, & a
    copyright section matching the new design.
    
    Note that this requires adding a new mailpoet form with First/Last Name
    & Email fields.
    
    The partners section will be brought back as a shortcode on the
    homepage.

--- footer.php
+++ footer.php
@@ -1,133 +1,91 @@
 <!-- div#body-wrapper is opened -->
-  <!-- Show Membership Info, Newsletter Signup, & Social Icons --><?php
-  $excluded_membership_footer_posts = array(
-    'new-listing', 'contact-fic', 'place-ad', 'contact-communities-magazine',
-    'contact-a-community', 'support',
-  );
-  $is_shop_but_not_homepage = is_woocommerce() && !is_shop();
-  if (!($is_shop_but_not_homepage || in_array(get_post()->post_name, $excluded_membership_footer_posts))) { ?>
-  <div id='membership-footer' class='row'>
-    <div class="bg-info p-3">
-      <div class="row">
-        <div class="col-md-16 col-lg-18">
-          <h2 class="text-center">Connect to the Communities Movement</h2>
-        </div>
-        <div class="col-md-8 col-lg-6"></div>
-        <!-- Left Section -->
-        <div class="col-md-16 col-lg-18 mb-4 mb-md-0">
-          <div class="card">
-            <div class="card-block">
-              <div class="row">
-                <div class="col-sm-10 d-flex">
-                  <a class="m-auto" href="/community-bookstore/product/fic-membership/" target="_blank">
-                    <img class="img-fluid alignnone" src="/wp-content/images/fic-membership-badge.png" alt="" width="288" height="259" />
-                  </a>
-                </div>
-                <div class="col-sm-14">
-                  <p class="text-primary font-weight-bold">
-                    Support the development of intentional communities and the
-                    evolution of cooperative culture.
-                  </p>
-                  <p>
-                    <a href="/community-bookstore/product/fic-membership/">Become an FIC Member</a>
-                    to receive special updates, webinars, reports, and discounts to
-                    books, events, ads, and more.
-                  </p>
-                  <p>
-                    IC.org is a project of the Fellowship for Intentional Community, a
-                    501(c)3 nonprofit organization.
-                  </p>
-                  <p>
-                    Your <a href="/community-bookstore/product/fic-membership/">Membership</a>
-                    and <a href="/support/">donations</a> are tax deductible. Please help to
-                    further this mission -- together we can change the world!
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!-- Right Section -->
-        <div class="col-md-8 col-lg-6">
-          <!-- Newsletter Form -->
-          <div style="width: 100%;"><?php echo do_shortcode('[wd_hustle id="fic-newsletter-homepage"]'); ?></div>
 
-          <!-- Social Icons -->
-          <div class="text-center">
-            <a class="mr-2" href="https://www.facebook.com/FellowshipForIntentionalCommunity">
-              <img alt="Facebook" style="max-width: 20%;" src="/wp-content/uploads/2018/01/fb_logo.png" />
-            </a>
-            <a class="mx-auto" href="https://www.youtube.com/channel/UC_bjpd6qhVcA1SuuF8pLRIg/featured">
-              <img alt="YouTube" style="max-width: 20%;" src="/wp-content/uploads/2018/01/youtube_logo.jpg" />
-            </a>
-            <a class="ml-2" href="https://twitter.com/iCdotOrg">
-              <img alt="Twitter" style="max-width: 20%;" src="/wp-content/uploads/2018/01/twitter_logo.png" />
-            </a>
-          </div>
-        </div>
-      </div>
+  <!-- Footer Support -->
+  <div id='footer-support' class='row'>
+    <div class='col-md-14'>
+      <h3>Our work is made possible thanks to community lovers like you!</h3>
+      <p>
+        Donate to support a thriving communities movement and become a FIC
+        Member for a host of special membership benefits.
+      </p>
+      <a class='btn btn-primary' href='/support/' target='_blank'>SUPPORT</a>
+    </div>
+    <div class='col-md-8'>
+      <img src='//placekitten.com/600/600' class='img-fluid' />
     </div>
   </div>
-  <?php } ?>
 
 
-  <!-- Partners -->
-  <?php $footer_logo_path = get_stylesheet_directory_uri() . "/img/footer-logos/"; ?>
-  <!-- Track Clicks with Google Analytics --><?php
-  function fic_footer_track_click($event_label) {
-    return "ga('send', 'event', 'Theme.Footer', 'click', '{$event_label}');";
-  }
-  ?>
-  <hr />
-  <div id="partners-footer" class="row">
-    <div class="col">
-      <div class="text-center mt-1">
-        We happily link to the following organizations, all of whom share our strong commitment to promoting community and a more cooperative world:
-        <div style="margin-top:12px;margin-bottom:8px;">
-          <a href="http://www.cohousing.org" title="Cohousing" target="_blank" onclick="<?php echo fic_footer_track_click('sister_org_coho'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>cohousing.png" alt="Cohousing" title="Cohousing" width="98" height="40" hspace="4" border="0">
-          </a>
-            <a href="http://TheFEC.org/" target="_blank" onclick="<?php echo fic_footer_track_click('sister_org_fec'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>fec.png" alt="The Federation of Egalitarian Communities - Communes Coop Community Cooperative Sustainable Intentional" title="The Federation of Egalitarian Communities - Sustainable Intentional Communes Coop Community Cooperative" width="119" height="40" border="0">
-          </a>
-            <a href="http://www.nasco.coop/" target="_blank" onclick="<?php echo fic_footer_track_click('sister_org_NASCO'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>nasco.jpg" alt="North American Students of Cooperation" title="North American Students of Cooperation" width="105" height="40" hspace="4" border="0">
-          </a>
-            <a href="http://gen.ecovillage.org" target="blank" onclick="<?php echo fic_footer_track_click('sister_org_GEN'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>gen.png" alt="Global Ecovillage Network" title="Global Ecovillage Network" width="168" height="40" hspace="4" border="0">
-          </a>
-            <a href="https://neweconomy.net/" target="blank" onclick="<?php echo fic_footer_track_click('sister_org_NEC'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>nec.png" alt="New Economy Coalition" title="New Economy Coalition" width="212" height="40" hspace="4" border="0">
-          </a>
-        </div>
-        <div class="mt-1 mb-1">
-          <a href="http://www.numundo.org/" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_NM'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>numundo.png" alt="NuMundo" title="NuMondo" height="40" hspace="4" border="0">
-          </a>
-            <a href="http://www.calcoho.org/" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_calcoho'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>calcoho.jpg" alt="California Cohousing" title="California Cohousing" height="40" hspace="4" border="0">
-          </a>
-            <a href="http://www.communa.org.il" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_ICD'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>icd.jpg" alt="International Communes Desk" title="International Communes Desk" height="40" hspace="4" border="0">
-          </a>
-            <a href="http://www.communa.org.il/icsa" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_ICSA'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>icsa.jpg" alt="ICSA" title="ICSA" height="40" hspace="4" border="0">
-          </a>
-            <a id="csa-footer-logo" href="http://www.communalstudies.org/" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_CSA'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>csa.gif" alt="Communal Studies Association" title="Communal Studies Association" height="50" hspace="4" border="0">
-          </a>
-            <a href="http://www.transitionus.org/" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_TransitionUS'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>transition-us.png" alt="Transition US" title="Transition US" height="40" hspace="4" border="0">
-          </a>
-            <a href="https://nwcommunities.org/" target="blank" onclick="<?php echo fic_footer_track_click('sponsor_NICA'); ?>">
-            <img src="<?php echo $footer_logo_path; ?>nica.jpg" alt="NW Intentional Communities Association" title="NW Intentional Communities Association" width="114" height="40" hspace="4" border="0">
-          </a>
-        </div>
+  <!-- Footer Columns -->
+  <div id='footer-columns' class='row'>
+    <div class='col-md-10 col-lg-8 left-column'>
+      <img src='//placekitten.com/700/300' class='img-fluid' />
+      <h5>OUR MISSION</h5>
+      <p>
+        To support and promote the development of intentional communities, as a
+        pathway towards a more sustainable and just world.
+      </p>
+      <a class='btn btn-primary' href='/about/'>LEARN MORE</a>
+    </div>
+    <div class='col-md-5 col-lg-4 middle-column'>
+      <h5>SITE PAGES</h5><?php
+      $footer_links = array(
+        array('title' => 'About FIC', 'link' => '/about/'),
+        array('title' => 'Directory', 'link' => '/directory/'),
+        array('title' => 'Membership', 'link' => '/membership/'),
+        array('title' => 'Donate', 'link' => '/donate/'),
+        array('title' => 'Communities Directory', 'link' => '/directory/'),
+        array('title' => 'Bookstore', 'link' => '/communities-bookstore/'),
+        array('title' => 'Magazine', 'link' => '/communities-magazine/'),
+        array('title' => 'Planet Community', 'link' => '/planet-community/'),
+        array('title' => 'Events', 'link' => '/events/'),
+        array('title' => 'Classifieds', 'link' => '/community-classifieds/'),
+        array('title' => 'Knowledgebase', 'link' => '/knowledgebase/'),
+        array('title' => 'Blog', 'link' => '/blog/'),
+        array('title' => 'Contact', 'link' => '/contact-fic/'),
+        array('title' => 'Privacy', 'link' => '/privacy/'),
+      );
+      echo '<ul>';
+      foreach ($footer_links as $link) {
+        echo "<li><a href='{$link['link']}' target='_blank'>{$link['title']}</a></li>";
+      }
+      echo '</ul>';
+      ?>
+    </div>
+    <div class='col-md-8 col-xl-6 right-column'>
+      <h5>NEWSLETTER</h5>
+      <p class='font-weight-normal'>Community movement news in your inbox!</p>
+      <?php echo do_shortcode('[mailpoet_form id="2"]'); ?>
+      <div class='text-right icons'>
+        <a href='https://www.facebook.com/FellowshipForIntentionalCommunity/' target='_blank'>
+          <i class='fab fa-2x fa-facebook-square'></i>
+        </a>
+        <a href='https://www.youtube.com/channel/UC_bjpd6qhVcA1SuuF8pLRIg' target='_blank'>
+          <i class='fab fa-2x fa-youtube'></i>
+        </a>
+        <a href='/contact-fic/' target='_blank'>
+          <i class='fa fa-2x fa-paper-plane'></i>
+        </a>
       </div>
     </div>
   </div>
 
-</div>
+
+  <!-- Copyright -->
+  <div id='footer-copyright' class='meta'>
+    <p>
+      Foundation for Intentional Community (FIC) is a registered 501(c)(3)
+      non-profit organization. Our main office is located at Dancing Rabbit
+      Ecovillage, MO, USA.
+      <br />
+      Stewarding the communities movement for over 30 years!
+    </p>
+
+    <div>© 2019 Foundation for Intentional Community. All rights reserved.</div>
+  </div>
+
+
+</div><!-- div#body-wrapper -->
 <?php wp_footer(); ?>
 </body>
 </html>
