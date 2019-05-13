5e8b751579bd4dfb19ece28a776e0a29f87f4b0d
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-05-13 14:38:06 -0400

    Move Directory Homepage JS Out of the Post Content
    Add the custom javascript that was in the Directory homepage's post
    content into the theme's directory JS file.

--- src/directory.js
+++ src/directory.js
@@ -6,6 +6,7 @@ import { Elm } from './Directory/Main.elm';
 
 var $ = window.jQuery;
 
+// Elm Application
 $(document).ready(function() {
   var node = document.getElementById('elm-directory');
   if (node) {
@@ -43,4 +44,42 @@ $(document).ready(function() {
       app.ports.onUrlChange.send(location.href);
     })
   }
+});
+
+// Directory Homepage
+$(document).ready(function() {
+  var nodes = document.getElementsByClassName('page-id-17');
+  if (nodes.length === 0) {
+    return;
+  }
+
+  /* Set the Community Status When Toggling the Establish / Forming Checkboxes */
+  var $hidden_input = $("input[name='community_status']");
+  var $established = $("input[name='is_established']");
+  var $forming = $("input[name='is_forming']");
+
+  var established_checked = $established[0].checked;
+  var forming_checked = $forming[0].checked;
+  updateHiddenInput($hidden_input);
+
+  $established.change(function(ev) {
+    established_checked = ev.target.checked;
+    updateHiddenInput($hidden_input);
+  });
+  $forming.change(function(ev) {
+    forming_checked = ev.target.checked;
+    updateHiddenInput($hidden_input);
+  });
+
+  function updateHiddenInput($hidden) {
+    var value = [];
+    if (established_checked) {
+      value.push("Established");
+    }
+    if (forming_checked) {
+      value.push("Forming, Re-forming");
+    }
+    $hidden[0].value = value.join(", ");
+  }
+
 });
