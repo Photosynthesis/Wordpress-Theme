2a4d76d794763ccce6137a99c4f76f94563f5b37
Pavan Rikhi <pavan.rikhi@gmail.com>
2019-06-19 04:28:36 -0400

    [#1505] Disable Non-Membership Renewal Emails
    Disable all subscription renewal emails for all GiveWP forms except for
    the Membership form.
    
    Fixes #1505

--- includes/wpgive.php
+++ includes/wpgive.php
@@ -2,8 +2,19 @@
 /** Customizations for the WPGive Plugins **/
 class ThemeWPGive
 {
+  const membership_form_id = 280072;
+
+  /* Disable Renewal Emails for Donations From Every Form But Membership */
+  public static function disable_renewal_emails($send, $subscription_id, $notice_id) {
+    $subscription = new Give_Subscription($subscription_id);
+    if ($subscription->form_id === self::membership_form_id) {
+      return true;
+    }
+    return false;
+  }
 }
 
 
+add_filter('give_recurring_send_renewal_reminder', array('ThemeWPGive', 'disable_renewal_emails'), 20, 3);
 
 ?>
